8cf487678d67a78ddb4a17987faeb7c52a57fefb
Dinobi <architectstan01@gmail.com>
2024-09-25 15:20:01 +0100

    updated some changes

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,12 +1,12 @@
 from flask import Flask, request, jsonify
 from api.v1.views import app_look
 from database.db import KingsRecordDatabase
-from flask_cors import CORS
+from flask_cors import CORS, cross_origin
 from flask_jwt_extended import JWTManager
 import os
 
 app = Flask(__name__)
-cors = CORS(app, resources={r"/*": {"origins": "*"}}, methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"])
+cors = CORS(app, resources={r"/*": {"origins": "*"}}, methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"], allow_headers=["Authorization", "Content-Type"])
 jwt = JWTManager(app)
 app.register_blueprint(app_look)
 app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY', 'secret_key')
@@ -22,6 +22,7 @@ else:
 
 
 @app.errorhandler(404)
+@cross_origin()
 def notFound(err):
     return jsonify({'error': f'Not found {err}'}), 404
 
