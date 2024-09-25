f978f2a35d6d8028f1d63dca0631a45518ab6cda
Dinobi <architectstan01@gmail.com>
2024-09-25 15:33:23 +0100

    updated some changes

--- models/Givings.py
+++ models/Givings.py
@@ -3,17 +3,13 @@ import datetime
 
 class Givings(mongoengine.EmbeddedDocument):
     """Giving model for a user"""
-    type = mongoengine.StringField(required=True, choices=['offering', 'tithes', 'specialSeeds', 'donation'])
+    type = mongoengine.StringField(required=True, choices=['Offering', 'Tithes', 'Special Seeds', 'Donation'])
     amount = mongoengine.IntField(default=0)
     createdAt = mongoengine.DateTimeField(default=datetime.datetime.now)
     updatedAt = mongoengine.DateTimeField(default=datetime.datetime.now)
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.offering = kwargs.get('Offering', False)
-        self.tithes = kwargs.get('Tithes', False)
-        self.specialSeeds = kwargs.get('Special Seeds', False)
-        self.donation = kwargs.get('Donations', False)
         self.amount = kwargs.get('amount', 0)
         self.createdAt = kwargs.get('createdAt', datetime.datetime.now)
         self.updatedAt = kwargs.get('updatedAt', datetime.datetime.now)
--- models/Partnership.py
+++ models/Partnership.py
@@ -2,18 +2,13 @@ import mongoengine
 import datetime
 
 class Partnership(mongoengine.EmbeddedDocument):
-    type = mongoengine.StringField(required=True, choices=['rhapsodyOfRealities', 'healingStreams', 'campusMinistry', 'innerCityMission', 'ministryProgram'])
+    type = mongoengine.StringField(required=True, choices=['Rhapsody of Realities', 'Healings Streams', 'Campus Ministry', 'InnerCity Missions', 'Ministry Programs'])
     amount = mongoengine.IntField(default=0)
     createdAt = mongoengine.DateTimeField(default=datetime.datetime.now)
     updatedAt = mongoengine.DateTimeField(default=datetime.datetime.now)
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.rhapsodyOfRealities = kwargs.get('Rhapsody of Realities', False)
-        self.healingStreams = kwargs.get('Healings Streams', False)
-        self.campusMinistry = kwargs.get('Campus Ministry', False)
-        self.innerCityMission = kwargs.get('InnerCity Missions', False)
-        self.ministryProgram = kwargs.get('Ministry Programs', False)
         self.amount = kwargs.get('amount', 0)
         self.createdAt = kwargs.get('createdAt', datetime.datetime.now)
         self.updatedAt = kwargs.get('updatedAt', datetime.datetime.now)
