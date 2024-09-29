ffce40a9db88652fc7c4286ac3ceaf428be193c4
Dinobi <architectstan01@gmail.com>
2024-09-29 03:06:35 +0100

    Added new updates

--- api/v1/views/recordData.py
+++ api/v1/views/recordData.py
@@ -25,8 +25,9 @@ def add_partnership():
             'admin': admin_id 
         }
         partnerships = data.get('partnerships', [])
+        Date = data.get('Date')
 
-        if not kwargs['firstName'] or not kwargs['lastName'] \
+        if not kwargs['firstName'] or not kwargs['lastName'] or not Date \
         or not kwargs['email'] or not kwargs['phoneNumber']:
             return jsonify({'error': 'All fields are required'}), 400
         if not partnerships :
@@ -43,15 +44,12 @@ def add_partnership():
                     if 'type' not in partner or 'amount' not in partner:
                         return jsonify({'error': 'Invalid partnership format'}), 400
                     type_details, amount = partner['type'], partner['amount']
-                    date = partner.get('Date')  # Get the date from the partner entry
-                    if not date:
-                        return jsonify({'error': 'Date is required for partnership'})
                     existing_partnership = next((p for p in chk_user.partnership if p.type == type_details), None)
                     if existing_partnership:
                         existing_partnership.amount += amount
                         existing_partnership.updatedAt = datetime.now()
                     else:
-                        chk_user.add_partnership(type_details, amount, date, createdAt=datetime.now())
+                        chk_user.add_partnership(type_details, amount, Date, createdAt=datetime.now())
             chk_user.save()
             return jsonify({'message': 'Partnership added successfully'}), 201
         else:
@@ -77,8 +75,9 @@ def add_givings():
             'admin': admin_id 
         }
         givings = data.get('givings', [])
+        Date = data.get('Date')
 
-        if not kwargs['firstName'] or not kwargs['lastName'] \
+        if not kwargs['firstName'] or not kwargs['lastName'] or not Date \
         or not kwargs['email'] or not kwargs['phoneNumber']:
             return jsonify({'error': 'All fields are required'}), 400
         if not givings :
@@ -95,15 +94,12 @@ def add_givings():
                     if 'type' not in partner or 'amount' not in partner:
                         return jsonify({'error': 'Invalid partnership format'}), 400
                     type_details, amount = partner['type'], partner['amount']
-                    date = partner.get('Date')  # Get the date from the partner entry
-                    if not date:
-                        return jsonify({'error': 'Date is required for partnership'})
                     existing_givings = next((p for p in chk_user.givings if p.type == type_details), None)
                     if existing_givings:
                         existing_givings.amount += amount
                         existing_givings.updatedAt = datetime.now()
                     else:
-                        chk_user.add_giving(type_details, amount, date, createdAt=datetime.now())
+                        chk_user.add_giving(type_details, amount, Date, createdAt=datetime.now())
             chk_user.save()
             return jsonify({'message': 'Givings added successfully'}), 201
         else:
